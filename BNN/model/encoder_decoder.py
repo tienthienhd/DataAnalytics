6ea611615201ae36b6bc912008b47124e9570cc6
tienthien <tienthienhd@gmail.com>
2018-08-17 13:30:02 +0700

    remove print detail epoch

--- BNN/model/encoder_decoder.py
+++ BNN/model/encoder_decoder.py
@@ -211,7 +211,7 @@ class EncoderDecoder(object):
 #                         avg_loss_train))
 #                print('interval time:', 
 #                      datetime.now()-start_time)
-					pass
+				    pass
                 
                 if self.early_stop(val_losses, 5):
                     print('finished training at epoch', epoch)
